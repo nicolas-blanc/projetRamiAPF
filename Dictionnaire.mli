796079498dea9d20e150d3c29320446a0e556531
nicolas.r.blanc@gmail.com <nicolas.r.blanc@gmail.com>
2014-12-19 23:13:14 +0100

    Ajout des commentaires dans RRami.mli et Dictionnaire.mli

--- Dictionnaire.mli
+++ Dictionnaire.mli
@@ -1,8 +1,39 @@
-    type dico = Noeud of dico array * bool | Feuille
-    val dico_vide :unit -> dico
-    val member : string -> dico -> bool
-    val add : string -> dico -> dico
-    val remove : string -> dico -> dico
-    val to_list : dico -> string list
-    val of_stream : char Stream.t -> dico
- 
+type dico = Noeud of dico array * bool | Feuille
+
+  (* Fonction qui creer un nouveau tableau vide
+   * J'ai ajouté le type unit en entrée pour en faire une fonction qui renvoie un tableau vide, cela permet de faciliter l'ajout d'un tableau vide et est plus facile d'utilisation et de propreté que d'appeller à chaque fois Arraymake
+   * Entrée : unit
+   * Sortie : Un nouveau Noeud composé d'un tableau de Feuille et False
+   *)
+val dico_vide : unit -> dico
+
+  (* Permet de tester si un mot est présent dans le dictionnaire en utilisant ou non des jokers pour le test du mot
+   * Entrées : Le mot à chercher - Le dico dans lequel cherché le mot
+   * Sortie : Retourne vrai si le mot est présent dans le dictionnaire, ou si un mot approchant avec un joker est possible dans le dictionnaire
+   *)
+val member : string -> dico -> bool
+
+  (* Permet d'ajouter un nouveau mot dans le dictionnaire, il ne faut pas qu'il y ai de lettres spéciales, mais la casse est indifférente
+   * Entrées : Le mot à ajouter - Le dico dans lequel l'ajouter
+   * Sortie : Renvoie le dico avec le mot ajouté
+   *)
+val add : string -> dico -> dico
+
+  (* Permet d'enlever un mot du dictionnaire, pas de lettres spéciales mais casse indifférente
+   * Entrées : Le mot à enlever - Le dico dans lequel le mot est enlevé
+   * Sortie : Renvoi le dico sans le mot
+   *)
+val remove : string -> dico -> dico
+
+  (* Permet de transformer un dictionnaire en une liste ordonné selon un ordre lexicographique
+   * Entrée : Le dictionnaire à transformer en liste
+   * Sortie : Une liste de mot ordonné selon l'ordre lexicographique
+   *)
+val to_list : dico -> string list
+
+  (* Permet de créer un dictionnaire à partir d'un flux de caractère où chaque ligne est un mot séparé par un retour à la ligne sans espace dans le flux
+   * Entrée : Le flux de caractère
+   * Sortie : Renvoie un dictionnaire contenant tous les mots du flux
+   *)
+val of_stream : char Stream.t -> dico
+  
--- RRami.mli
+++ RRami.mli
@@ -3,10 +3,39 @@ open REGLE
 
 module TRami = 
 sig
+  (* Permet de compter le nombre d'éléments dans une liste - Equivalent à List.Length
+   * Entrée : Liste d'élément
+   * Sortie :   
+   *)
   val nbElement : 'a list -> int
-  val lettreMain : main -> main -> combi
+
+  (* Permet de verifier si la combinaison appartient appartenait a la main du joueur -> Ancienne Main = Combi + Nouvelle main
+   * Entrées : Main du joueur - Nouvelle Main du joueur - Combinaison de lettres à mettre sur le jeux
+   * Sortie : renvoie vrai si la combinaison appartient seulement à la main du joueur
+   *)
+  val lettreMain : main -> main -> combi -> bool
+
+  (* Permet de tester si deux combinaisons sont égales
+   * Entrée : une combinaison de lettres - Une autre combinaison de lettres
+   * Sortie : Renvoie vrai si les deux combinaison sont les même
+   *)
   val equalCombi : combi -> combi -> bool
+
+  (* Permet de tester si une combinaison appartient à une liste de combinaison
+   * Entrées : Une combinaison de lettres - une liste de combinaison
+   * Sortie : Renvoie vrai si combi appartient à la liste de combi
+   *)
   val appMot : combi -> combi list -> bool
+
+  (* Renvoie une liste de combinaison contenant toutes les combinaisons de la deuxième liste de combinaison qui ne sont pas dansla première combinaison
+   * Entrées : Une combinaison de liste - Une autre combinaison de liste
+   * Sortie : Renvoie une liste de combinaison contenant tout les mots de la seconde liste sans les mots de la première liste
+   *)
   val longMot : combi list -> combi list -> combi list
+
+  (* Calcul du nombre d'élément de la plus grande combinaison dans la liste de combinaison
+   * Entrée : Une combinaison de liste
+   * Sortie : Nombre d'élément de la plus grande combinaison de la liste de combinaison
+   *)
   val calculPlusGrandMot : combi list -> int
 end
